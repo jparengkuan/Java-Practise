3ccb9e621b8573ab5f5c0dbf8feb24bc4e0c74eb
FJ Parengkuan <jim.parengkuan@gmail.com>
2019-05-08 21:10:46 +0200

    Queues

---
+++ src/Collections/QueueExample.java
@@ -0,0 +1,36 @@
+package Collections;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class QueueExample {
+
+    public static void main(String[] args) {
+
+        Queue<String> waitingQueue = new LinkedList<>();
+        waitingQueue.add("Een");
+        waitingQueue.add("twee");
+        waitingQueue.add("drie");
+
+        System.out.println("waitingQueue " + waitingQueue);
+
+        System.out.println(waitingQueue.remove());
+
+        System.out.println("New waitingQueue " + waitingQueue);
+
+
+
+        PriorityQueue<String> brands = new PriorityQueue<>();
+        brands.add("Samsung");
+        brands.add("Apple");
+        brands.add("Lg");
+
+        System.out.println("PriorityQueue " + brands);
+
+
+
+
+
+    }
+}
